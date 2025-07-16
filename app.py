e898aa4129a523d9112b1b8cc290e1fedc93eea0
sintik <sintik_souza@hotmail.com>
2025-07-15 22:12:46 -0300

    teste

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.chat_models import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Chave da OpenAI
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-# Carregar e processar o PDF
-loader = PyPDFLoader("arquivos/500perguntasabacaxi.pdf")
-documents = loader.load()
-splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-chunks = splitter.split_documents(documents)
-
-
+# Carregando o índice FAISS salvo no Colab
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-vectorstore = FAISS.from_documents(chunks, embeddings)
-retriever = vectorstore.as_retriever()
+db = FAISS.load_local("meu_index", embeddings, allow_dangerous_deserialization=True)
+retriever = db.as_retriever()
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
@@ -35,6 +26,6 @@ def chat():
     return jsonify({"answer": resposta})
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
